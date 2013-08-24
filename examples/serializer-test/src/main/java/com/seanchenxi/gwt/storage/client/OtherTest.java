1b2976a1cf9acee5e6912fc0363238e5c2ceb095
Xi Chen <chenxifr@gmail.com>
2013-08-24 17:32:47 +0200

    reorganized project folders to separate test using classes

---
+++ examples/serializer-test/src/main/java/com/seanchenxi/gwt/storage/client/OtherTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2013 Xi CHEN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.seanchenxi.gwt.storage.client;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+
+/**
+ * Created by: Xi
+ */
+public class OtherTest extends StorageTestUnit {
+
+  public static void removeValue(StorageExt storage) throws StorageQuotaExceededException, SerializationException {
+    final int initialSize = storage.size();
+    final StorageKey<Integer> key = StorageKeyFactory.intKey("removeValue1");
+    final Integer value1 = Integer.MAX_VALUE;
+    final Integer value2 = Integer.MIN_VALUE;
+
+    storage.put(key, value1);
+    storage.put(StorageKeyFactory.intKey("removeValue2"), value2);
+    assertTrue("removeValue - containsKey", storage.containsKey(key));
+    assertTrue("removeValue - containsKey", storage.containsKey(StorageKeyFactory.intKey("removeValue2")));
+
+    storage.remove(key);
+    storage.remove(StorageKeyFactory.intKey("removeValue2"));
+    assertEquals("removeValue - storage size", initialSize, storage.size());
+    assertTrue("removeValue - doesn't containsKey", !storage.containsKey(key));
+    assertTrue("removeValue - doesn't containsKey", !storage.containsKey(StorageKeyFactory.intKey("removeValue2")));
+  }
+
+  public static HandlerRegistration listenerTest(final StorageExt storage, StorageChangeEvent.Level level){
+    storage.setEventLevel(level);
+    trace("Storage Event Level set to " + level);
+    return storage.addStorageChangeHandler(new StorageChangeEvent.Handler() {
+      @Override
+      public void onStorageChange(StorageChangeEvent event) {
+        event("onStorageChange - Type=" + event.getChangeType() + ", " +
+          "Key=" + event.getKey() + ", " +
+          "Data=" + event.getData() + ", " +
+          "OldData=" + event.getOldData() + ", " +
+          "Value=" + String.valueOf(event.getValue()) + ", " +
+          "OldValue=" + String.valueOf(event.getOldValue()), false);
+        if(event.getKey() != null){
+          String value = storage.getString(event.getKey().name());
+          String expected = event.getData();
+          if ((expected == null && value == null) || (expected != null && expected.equals(value)))
+            event("onStorageChange - assertEquals succeed.", false);
+          else
+            event("onStorageChange -  assertEquals error: expected=" + event.getData() + ", but given=" + value, true);
+        }
+        event("==", false);
+      }
+    });
+  }
+}
---
+++ examples/serializer-test/src/main/java/com/seanchenxi/gwt/storage/client/TestValue.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Xi CHEN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.seanchenxi.gwt.storage.client;
+
+import java.io.Serializable;
+
+/**
+ * Created by: Xi
+ */
+public class TestValue implements Serializable {
+
+  private String value;
+
+  public TestValue(){}
+
+  public TestValue(String value){
+    this.value = value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof TestValue)) return false;
+
+    TestValue testValue = (TestValue) o;
+
+    return value == null ? (testValue.value == null) : value.equals(testValue.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return value.hashCode();
+  }
+}
